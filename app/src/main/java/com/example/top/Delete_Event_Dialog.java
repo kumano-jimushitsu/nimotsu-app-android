63d0e2a18f9fc43d562913c1ea7ce4ad2ade8430
clippa-kumano <clippa_core@outlook.jp>
2022-02-05 21:07:51 +0900

    EventDialogをこれからいじる（いまは動いてない）

--- app/src/main/java/com/example/top/Delete_Event_Dialog.java
+++ app/src/main/java/com/example/top/Delete_Event_Dialog.java
@@ -7,11 +7,12 @@ import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+import androidx.fragment.app.DialogFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 
 public class Delete_Event_Dialog extends DialogFragment {
     String event_id = "";
@@ -44,19 +45,38 @@ public class Delete_Event_Dialog extends DialogFragment {
                 sql = "select * from parcels where uid ='"+ parcel_id + "'";
                 cursor = db.rawQuery(sql, null);
                 cursor.moveToFirst();
-                message += "(" + cursor.getString(cursor.getColumnIndex("register_datetime")) +")";
+
                 message +="\r\n";
                 message +="　・荷物の所有者："+ cursor.getString(cursor.getColumnIndex("owner_room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("owner_ryosei_name"));
-                message +="\r\n";
-                message +="　・受取事務当　："+ cursor.getString(cursor.getColumnIndex("register_staff_room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("register_staff_ryosei_name"));
                 message +="\r\n";
                 message +="　・引き渡し状況：";
                 if(cursor.getInt(cursor.getColumnIndex("is_released"))==0){
                     message+="未引渡";
+                    //ついでに、ここから引渡のダイアログを出せるようにする
+                    builder.setNeutralButton("受取",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            DialogFragment dialogFragment = new Nimotsu_Touroku_Dialog();
+                            String[] newStr = owner_room_name.split("\\s+");
+                            Bundle args = new Bundle();
+                            args.putString("owner_room",newStr[0]);
+                            args.putString("owner_name",newStr[1]);
+                            args.putString("owner_id",owner_id);
+                            args.putString("register_staff_room",jimuto_room_Str);
+                            args.putString("register_staff_name",jimuto_name_Str);
+                            args.putString("register_staff_id",jimuto_id_Str);
+                            dialogFragment.setArguments(args);
+                            dialogFragment.show(getFragmentManager(), "Nimotsu_Touroku_Dialog");
+                        }
+                    });
                 }else{
                     message+="引渡済";
                 }
                 message +="\r\n";
+                message +="　・寮到着時間　：" + cursor.getString(cursor.getColumnIndex("register_datetime")).replace('-','/') +"";
+                message +="\r\n";
+                message +="　・受取時事務当　："+ cursor.getString(cursor.getColumnIndex("register_staff_room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("register_staff_ryosei_name"));
+                message +="\r\n";
+
                 message +="　・荷物の種類　：";//{"普通", "冷蔵", "冷凍","大型","不在票","その他"};
                 if(cursor.getInt(cursor.getColumnIndex("placement"))==0)message+="一般";
                 if(cursor.getInt(cursor.getColumnIndex("placement"))==1)message+="冷蔵";
@@ -66,12 +86,38 @@ public class Delete_Event_Dialog extends DialogFragment {
                 if(cursor.getInt(cursor.getColumnIndex("placement"))==5){
                     message+="その他 memo:"+cursor.getString(cursor.getColumnIndex("note")) ;
                 }
-
-
-
                 break;
             case "2"://引渡
-                message = "";
+                message = "寮生に荷物を引き渡しました。";
+                sql = "select * from parcels where uid ='"+ parcel_id + "'";
+                cursor = db.rawQuery(sql, null);
+                cursor.moveToFirst();
+
+                message +="\r\n";
+                message +="　・荷物の所有者："+ cursor.getString(cursor.getColumnIndex("owner_room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("owner_ryosei_name"));
+                message +="\r\n";
+                message +="　・寮到着時間　：" + cursor.getString(cursor.getColumnIndex("register_datetime")).replace('-','/') +"";
+                message +="\r\n";
+                message +="　・寮生引渡時間：" + cursor.getString(cursor.getColumnIndex("release_datetime")).replace('-','/') +"";
+                message +="\r\n";
+                message +="　・引渡時事務当："+ cursor.getString(cursor.getColumnIndex("register_staff_room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("register_staff_ryosei_name"));
+                message +="\r\n";
+
+                message +="　・荷物の種類　：";//{"普通", "冷蔵", "冷凍","大型","不在票","その他"};
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==0)message+="一般";
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==1)message+="冷蔵";
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==2)message+="冷凍";
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==3)message+="大型";
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==4)message+="不在表";
+                if(cursor.getInt(cursor.getColumnIndex("placement"))==5){
+                    message+="その他 memo:"+cursor.getString(cursor.getColumnIndex("note")) ;
+                }
+                if(cursor.getString(cursor.getColumnIndex("release_agent_uid"))!=null){
+                    sql = "select room_name, ryosei_name from ryosei where uid ='"+ cursor.getString(cursor.getColumnIndex("release_agent_uid")) + "'";
+                    cursor = db.rawQuery(sql, null);
+                    message+="\r\n　・代理受取　　："+ cursor.getString(cursor.getColumnIndex("room_name")) +"　"+ cursor.getString(cursor.getColumnIndex("rryosei_name"));
+                }
+
                 break;
             case "10"://事務当交代
                 message = "事務当番交代　";
@@ -97,19 +143,28 @@ public class Delete_Event_Dialog extends DialogFragment {
         builder.setTitle("操作履歴　詳細情報")
                 .setMessage(message);
 
+
+
+
+        //受取か引渡で、is_finished=0ならば削除可能　削除ボタンを表示させる
         if(event_type.equals("1")||event_type.equals("2")){
-            builder.setPositiveButton("操作取り消し", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    SQLiteDatabase db = _helper.getWritableDatabase();
-                    _helper.delete_event(db,event_id,ryosei_id,parcel_id,event_type);
+            sql = "SELECT is_finished FROM parcel_event where uid ='" + event_id + "';";
+            cursor = db.rawQuery(sql, null);
+            cursor.moveToFirst();
+            if(cursor.getInt(cursor.getColumnIndex("is_finished")) ==0) {
+                builder.setPositiveButton("操作取り消し", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        SQLiteDatabase db = _helper.getWritableDatabase();
+                        _helper.delete_event(db, event_id, ryosei_id, parcel_id, event_type);
 //
 //                    update_parcels_shearingstatus();
 //                    update_ryosei_shearingstatus();
 //                    update_event_shearingstatus();
 //                    _helper.close();
-                }
-            })
-                    .setNegativeButton("キャンセル", null);
+                    }
+                });
+            }
+                    //.setNegativeButton("確認終了　", null);
         }
         return builder.create();
     }
@@ -125,6 +180,20 @@ public class Delete_Event_Dialog extends DialogFragment {
             alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextSize(20);
         }
 
+    }
+
+    public void showDialog(View view, String owner_room_name, String owner_id) {
+        DialogFragment dialogFragment = new Nimotsu_Uketori_Dialog();
+        String[] newStr = owner_room_name.split("\\s+");
+        Bundle args = new Bundle();
+        args.putString("owner_room",newStr[0]);
+        args.putString("owner_name",newStr[1]);
+        args.putString("owner_id",owner_id);
+        //args.putString("release_staff_room",jimuto_room_Str);
+        //args.putString("release_staff_name",jimuto_name_Str);
+        args.putString("release_staff_id",jimuto_id_Str);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "Nimotsu_Uketori_Dialog");
     }
     public void update_parcels_shearingstatus (){
 
